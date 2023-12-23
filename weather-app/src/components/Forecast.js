b07a18bc7cfc9bc4c89cf6c9c50d3e7cb4a37a18
jnkzd <uzasny.honzicek@seznam.cz>
2023-12-24 00:02:02 +0100

    Add method to populate ForecastTile with weatherData

--- weather-app/src/components/Forecast.js
+++ weather-app/src/components/Forecast.js
@@ -1,23 +1,29 @@
 import { useEffect } from "react";
-import useGetWeatherData from "./hooks"
+import useGetWeatherData from "./hooks";
 import ForecastTile from "./ForecastTile";
 
 const Forecast = () => {
-    const {weatherData, fetchData} = useGetWeatherData();
-    
-    useEffect(() => {
-     fetchData();
-    }, [])
+  const { weatherData, fetchData } = useGetWeatherData();
 
-    return(
-    <>
-    <h1>Forecast component</h1>
-    {weatherData ?
-    <>
-    <ForecastTile time={weatherData.list[0].dt_txt} temp={weatherData.list[0].main.temp} weather={weatherData.list[0].weather[0].description}/> 
-    <ForecastTile time={weatherData.list[1].dt_txt} temp={weatherData.list[1].main.temp} weather={weatherData.list[1].weather[0].description}/>
-    </> : <p>Loading</p>}
-    </>)
-}
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-export default Forecast
+  return (
+    <>
+      <h1>Forecast component</h1>
+      {weatherData ? (
+        <>
+          <h2>{weatherData.city.name}, {weatherData.city.country}</h2>
+          {weatherData.list.map((item, index) => (
+            <ForecastTile key={index} time={item.dt_txt} weather={item.weather[0].description} temp={item.main.temp}/>
+          ))}
+        </>
+      ) : (
+        <p>Loading</p>
+      )}
+    </>
+  );
+};
+
+export default Forecast;
--- weather-app/src/components/ForecastTile.js
+++ weather-app/src/components/ForecastTile.js
@@ -1,10 +1,12 @@
+import "./ForecastTile.css"
+
 const ForecastTile = ({time, weather, temp}) => {
   return (
-    <>
+    <div className="forecastTile">
       <p>{time}</p>
       <p>{weather}</p>
       <p>{temp}</p>
-    </>
+    </div>
   );
 };
 
