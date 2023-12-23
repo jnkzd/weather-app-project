f6920b8eeb1f4cd1cd761c7e0e2fd9a017f42b19
jnkzd <uzasny.honzicek@seznam.cz>
2023-12-24 00:47:36 +0100

    Add test input fields. Add variables to control longitude + latitude to fetch data for

--- weather-app/src/components/Forecast.js
+++ weather-app/src/components/Forecast.js
@@ -1,17 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useGetWeatherData from "./hooks";
 import ForecastTile from "./ForecastTile";
 
 const Forecast = () => {
   const { weatherData, fetchData } = useGetWeatherData();
+  const [lat, setLat] = useState();
+  const [long, setLong] = useState();
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const setCoords = () => {
+    console.log(lat + " " + long)
+    fetchData([lat, long])
+  }
+
+  const handleChange = (e) => {
+    let fieldName = e.target.name;
+    let fieldValue = e.target.value;
+
+    if (fieldName == "lat") {
+      setLat(fieldValue);
+      console.log(lat)
+    }
+
+    if (fieldName == "long") {
+      setLong(fieldValue);
+    }
+  }
+
+
 
   return (
     <>
       <h1>Forecast component</h1>
+      <input type="text" name="lat" onChange={handleChange} placeholder="Latitude"></input>
+      <p></p>
+      <input type="text" name="long" onChange={handleChange} placeholder="Longitude"></input>
+      <p></p>
+      <button onClick={setCoords}>Set coords</button>
       {weatherData ? (
         <>
           <h2>{weatherData.city.name}, {weatherData.city.country}</h2>
@@ -20,7 +44,7 @@ const Forecast = () => {
           ))}
         </>
       ) : (
-        <p>Loading</p>
+        <p></p>
       )}
     </>
   );
--- weather-app/src/components/hooks.js
+++ weather-app/src/components/hooks.js
@@ -3,9 +3,9 @@ import { useState } from "react";
 const useGetWeatherData = () => {
   const [weatherData, setWeatherData] = useState();
 
-  const fetchData = async () => {
+  const fetchData = async (coords) => {
     const res = await fetch(
-      'https://api.openweathermap.org/data/2.5/forecast?lat=49.593777&lon=17.250879&appid=7649f65dc6d3c6bc1e9f39e0f0e812ac&units=metric'
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${coords[0]}&lon=${coords[1]}&appid=7649f65dc6d3c6bc1e9f39e0f0e812ac&units=metric`
     );
     const data = await res.json();
     console.log(data);
