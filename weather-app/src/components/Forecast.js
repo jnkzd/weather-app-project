21aef0546f6c5c7ef9d1f32d9b440d70b78408c0
jnkzd <uzasny.honzicek@seznam.cz>
2023-12-25 00:01:26 +0100

    Add function to format time according to local settings

--- weather-app/src/components/Forecast.js
+++ weather-app/src/components/Forecast.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import useGetWeatherData from "./hooks";
 import ForecastTile from "./ForecastTile";
 
--- weather-app/src/components/ForecastTile.js
+++ weather-app/src/components/ForecastTile.js
@@ -3,9 +3,13 @@ import "./ForecastTile.css"
 const ForecastTile = ({time, weather, temp, icon}) => {
   const imageUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`
 
+  const formatTime = (unix) => {
+    return new Date(unix * 1000).toLocaleTimeString({hour: "2-digit", minute: "2-digit"}) 
+  }
+
   return (
     <div className="forecastTile">
-      <p>{time}</p>
+      <p>{formatTime(time)}</p>
       <img src={imageUrl}></img>
       <p>{weather}</p>
       <p>{temp}</p>
