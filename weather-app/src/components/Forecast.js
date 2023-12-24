b813420750f8b6440ca99ef179edc7ec0fdcc1af
jnkzd <uzasny.honzicek@seznam.cz>
2023-12-24 02:09:48 +0100

    Fix an issue with asynchronous lat and long setState

--- weather-app/src/components/Forecast.js
+++ weather-app/src/components/Forecast.js
@@ -4,43 +4,46 @@ import ForecastTile from "./ForecastTile";
 
 const Forecast = () => {
   const { weatherData, fetchData } = useGetWeatherData();
-  const [lat, setLat] = useState();
-  const [long, setLong] = useState();
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
 
   const setCoords = () => {
-    console.log(lat + " " + long)
-    fetchData([lat, long])
-  }
-
-  const handleChange = (e) => {
-    let fieldName = e.target.name;
-    let fieldValue = e.target.value;
-
-    if (fieldName == "lat") {
-      setLat(fieldValue);
-      console.log(lat)
-    }
-
-    if (fieldName == "long") {
-      setLong(fieldValue);
-    }
-  }
-
-
+    console.log(lat + " " + long);
+    fetchData([lat, long]);
+  };
 
   return (
     <>
       <h1>Forecast component</h1>
-      <input type="text" name="lat" onChange={handleChange} placeholder="Latitude"></input>
+      <input
+        type="text"
+        name="lat"
+        placeholder="Latitude"
+        value={lat}
+        onChange={(e) => setLat(e.target.value)}
+      ></input>
       <p></p>
-      <input type="text" name="long" onChange={handleChange} placeholder="Longitude"></input>
+      <input
+      value={long}
+        type="text"
+        name="long"
+        onChange={(e) => setLong(e.target.value)}
+        placeholder="Longitude"
+      ></input>
       <p></p>
       <button onClick={setCoords}>Set coords</button>
       {weatherData ? (
         <>
-          <h2>{weatherData.city.name}, {weatherData.city.country}</h2>
+          <h2>
+            {weatherData.city.name}, {weatherData.city.country}
+          </h2>
           {weatherData.list.map((item, index) => (
-            <ForecastTile key={index} time={item.dt_txt} weather={item.weather[0].description} temp={item.main.temp}/>
+            <ForecastTile
+              key={index}
+              time={item.dt_txt}
+              weather={item.weather[0].description}
+              temp={item.main.temp}
+            />
           ))}
         </>
       ) : (
