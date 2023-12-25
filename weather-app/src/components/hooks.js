f08645696947c2c7f000476d3ba0a632ab7f590f
jnkzd <uzasny.honzicek@seznam.cz>
2023-12-25 19:06:49 +0100

    Hotfix: replace hard-coded API key with .env file

--- weather-app/src/components/hooks.js
+++ weather-app/src/components/hooks.js
@@ -5,7 +5,7 @@ const useGetWeatherData = () => {
 
   const fetchData = async (coords) => {
     const res = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${coords[0]}&lon=${coords[1]}&appid=7649f65dc6d3c6bc1e9f39e0f0e812ac&units=metric`
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${coords[0]}&lon=${coords[1]}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
     );
     const data = await res.json();
     console.log(data);
